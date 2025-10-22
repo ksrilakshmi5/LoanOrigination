bfe3f8146aaed8c0ee3178698ad76c051825e429
ksrilakshmi5 <131759753+ksrilakshmi5@users.noreply.github.com>
2025-10-22 20:23:09 +0530

    Update LoanDetails.java

--- LoanDetails.java
+++ LoanDetails.java
@@ -1,12 +1,5 @@
 package com.loanorigination.backend.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class LoanDetails {
 
     private String loanType;              // Personal Loan, Home Loan, Vehicle Loan
@@ -14,9 +7,106 @@ public class LoanDetails {
     private Integer loanDuration;         // In years
     private String loanPurpose;           // Text description
 
-    // For document uploads (you can store file URLs or filenames)
+    // For document uploads (store file paths or names)
     private String homeSaleAgreement;     // File path or name
     private String homeEcDocument;
     private String vehicleInvoice;
     private String vehicleQuotation;
+
+    // --- Constructors ---
+    public LoanDetails() {
+    }
+
+    public LoanDetails(String loanType, Double loanAmount, Integer loanDuration, String loanPurpose,
+                       String homeSaleAgreement, String homeEcDocument,
+                       String vehicleInvoice, String vehicleQuotation) {
+        this.loanType = loanType;
+        this.loanAmount = loanAmount;
+        this.loanDuration = loanDuration;
+        this.loanPurpose = loanPurpose;
+        this.homeSaleAgreement = homeSaleAgreement;
+        this.homeEcDocument = homeEcDocument;
+        this.vehicleInvoice = vehicleInvoice;
+        this.vehicleQuotation = vehicleQuotation;
+    }
+
+    // --- Getters and Setters ---
+    public String getLoanType() {
+        return loanType;
+    }
+
+    public void setLoanType(String loanType) {
+        this.loanType = loanType;
+    }
+
+    public Double getLoanAmount() {
+        return loanAmount;
+    }
+
+    public void setLoanAmount(Double loanAmount) {
+        this.loanAmount = loanAmount;
+    }
+
+    public Integer getLoanDuration() {
+        return loanDuration;
+    }
+
+    public void setLoanDuration(Integer loanDuration) {
+        this.loanDuration = loanDuration;
+    }
+
+    public String getLoanPurpose() {
+        return loanPurpose;
+    }
+
+    public void setLoanPurpose(String loanPurpose) {
+        this.loanPurpose = loanPurpose;
+    }
+
+    public String getHomeSaleAgreement() {
+        return homeSaleAgreement;
+    }
+
+    public void setHomeSaleAgreement(String homeSaleAgreement) {
+        this.homeSaleAgreement = homeSaleAgreement;
+    }
+
+    public String getHomeEcDocument() {
+        return homeEcDocument;
+    }
+
+    public void setHomeEcDocument(String homeEcDocument) {
+        this.homeEcDocument = homeEcDocument;
+    }
+
+    public String getVehicleInvoice() {
+        return vehicleInvoice;
+    }
+
+    public void setVehicleInvoice(String vehicleInvoice) {
+        this.vehicleInvoice = vehicleInvoice;
+    }
+
+    public String getVehicleQuotation() {
+        return vehicleQuotation;
+    }
+
+    public void setVehicleQuotation(String vehicleQuotation) {
+        this.vehicleQuotation = vehicleQuotation;
+    }
+
+    // --- toString ---
+    @Override
+    public String toString() {
+        return "LoanDetails{" +
+                "loanType='" + loanType + '\'' +
+                ", loanAmount=" + loanAmount +
+                ", loanDuration=" + loanDuration +
+                ", loanPurpose='" + loanPurpose + '\'' +
+                ", homeSaleAgreement='" + homeSaleAgreement + '\'' +
+                ", homeEcDocument='" + homeEcDocument + '\'' +
+                ", vehicleInvoice='" + vehicleInvoice + '\'' +
+                ", vehicleQuotation='" + vehicleQuotation + '\'' +
+                '}';
+    }
 }
